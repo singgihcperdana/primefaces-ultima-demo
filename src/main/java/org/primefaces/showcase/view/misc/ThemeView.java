50198317e846453f4a8ab2ec097fa1b23358ca8b
ratking <ratking@ynet.com>
2016-10-09 18:28:26 +0800

    layoutmenu.xhtml改名为menu.xhtml，实现Customization下9个菜单项的功能

--- src/main/java/org/primefaces/showcase/view/misc/ThemeView.java
+++ src/main/java/org/primefaces/showcase/view/misc/ThemeView.java
@@ -29,13 +29,32 @@ public class ThemeView implements Serializable {
 
     /** 默认主题：ultima-indigo */
     public static final String DEFAULT_THEME = "ultima-indigo";
+    /** 默认字体样式：layout-compact */
+    public static final String DEFAULT_SIZE = "layout-compact";
+    /** 默认菜单位置样式：static */
+    public static final String DEFAULT_MENU_MODE = "static";
+    /** 默认菜单背景样式：menu-layout-static */
+    public static final String DEFAULT_MENU_BG = "";
+    /** 默认简介位置：inline */
+    public static final String DEFAULT_PROFILE = "inline";
     /** 布局的主题(其它可选主题：ultima-blue/ultima-blue-grey/ultima-brown/ultima-cyan/ultima-dark-blue/ultima-dark-green/ultima-green/ultima-grey/ultima-indigo/ultima-purple-amber/ultima-purple-cyan/ultima-teal) */
     private String theme;
     /** 布局的CSS样式文件(其它可选样式：layout-blue/layout-blue-grey/layout-brown/layout-cyan/layout-dark-blue/layout-dark-green/layout-green/layout-grey/layout-indigo/layout-purple-amber/layout-purple-cyan/layout-teal) */
     private String layoutCss;// = "css/layout-indigo.css";
     private String themeColor;// = "#3F51B5";
+    /** 字体样式：layout-compact或"" */
+    private String size;// = "layout-compact";
+    /** 菜单样式：static/overlay/horizontal */
+    private String menuMode;// = "static";
+    /** 菜单位置样式：layout-wrapper menu-layout-static/layout-wrapper menu-layout-overlay/layout-wrapper menu-layout-static menu-layout-horizontal */
+    private String menuModeClass;
+    /** 菜单背景样式：""/layout-menu-dark */
+    private String menuBg; // = "";
+    /** 简介位置(inline/top) */
+    private String profile; // = "inline";
     private Map<String, String> layoutMap;
     private Map<String, String> themeColorMap;
+    private Map<String, String> menuModeClassMap;
 
     @PostConstruct
     public void init() {
@@ -67,7 +86,16 @@ public class ThemeView implements Serializable {
         themeColorMap.put("ultima-purple-cyan", "#673AB7");
         themeColorMap.put("ultima-teal", "#009688");
 
+        menuModeClassMap = new HashMap<>();
+        menuModeClassMap.put("static", "layout-wrapper menu-layout-static");
+        menuModeClassMap.put("overlay", "layout-wrapper menu-layout-overlay");
+        menuModeClassMap.put("horizontal", "layout-wrapper menu-layout-static menu-layout-horizontal");
+
         setTheme(DEFAULT_THEME);
+        setSize(DEFAULT_SIZE);
+        setMenuMode(DEFAULT_MENU_MODE);
+        setMenuBg(DEFAULT_MENU_BG);
+        setProfile(DEFAULT_PROFILE);
     }
 
     public String getTheme() {
@@ -98,5 +126,51 @@ public class ThemeView implements Serializable {
 
     public void setThemeColor(String themeColor) {
         this.themeColor = themeColor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getMenuMode() {
+        return menuMode;
+    }
+
+    public void setMenuMode(String menuMode) {
+        this.menuMode = menuMode;
+
+        this.theme = theme;
+        menuModeClass = menuModeClassMap.get(menuMode);
+//        if (menuModeClass == null) {
+//            menuModeClass = "layout-wrapper menu-layout-static";
+//        }
+    }
+
+    public String getMenuModeClass() {
+        return menuModeClass;
+    }
+
+    public void setMenuModeClass(String menuModeClass) {
+        this.menuModeClass = menuModeClass;
+    }
+
+    public String getMenuBg() {
+        return menuBg;
+    }
+
+    public void setMenuBg(String menuBg) {
+        this.menuBg = menuBg;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
     }
 }
